baafeec3c9f518013d087518a46cfdf4e6544319
mukhtarabdirahman <mukhtarabdirahmamn@gmail.com>
2020-03-11 09:00:09 +0300

    upload form

---
+++ src/assets/js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+    $( "#datepicker" ).datepicker({
+        dateFormat : 'mm/dd/yy',
+        changeMonth : true,
+        changeYear : true,
+        yearRange: '-100y:c+nn',
+        maxDate: '-1d'
+    });
+});
