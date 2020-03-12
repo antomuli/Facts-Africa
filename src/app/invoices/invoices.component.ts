7c31bbc47d5b3ac9cfb76ecade3750e678d9ec46
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-03-12 09:22:53 +0300

    fix and reolve children componbent

--- src/app/invoices/invoices.component.ts
+++ src/app/invoices/invoices.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { InvoiceService } from '../services/invoice/invoice.service';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-invoices',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./invoices.component.css']
 })
 export class InvoicesComponent implements OnInit {
+  invoices = []
 
-  constructor() { }
+  constructor(private dataService: InvoiceService, private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
+    console.log("vendors component loaded")
+    this.dataService.getInvoices().subscribe( res => {
+      this.invoices = res
+    }, error => console.log(error))
+  }
+
+  
+
+  Logout =() => {
+    console.log("user is logging out")
+    localStorage.clear();
+    this.router.navigate(['/']);
+  }
+  Upload = () => {
+      this.router.navigate(['upload'])
   }
 
 }
