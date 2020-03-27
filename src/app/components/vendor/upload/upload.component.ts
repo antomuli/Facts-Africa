2d4819dc0de38e2b493844b5b3f052896791f9ac
kiprotichdominic <kiprotichdominic102@gmail.com>
2020-03-27 21:57:57 +0300

    fix upload

--- src/app/components/vendor/upload/upload.component.ts
+++ src/app/components/vendor/upload/upload.component.ts
@@ -5,6 +5,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { ViewbuyersService } from 'src/app/_services/buyers/viewbuyers.service';
 import { Invoice } from 'src/app/_models/invoice';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-upload',
@@ -12,32 +13,46 @@ import { Invoice } from 'src/app/_models/invoice';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent implements OnInit {
-
+  submitted = false;
   invoice = Invoice;
   buyers =  []
 
   uploadForm = new FormGroup({
     buyer_id: new FormControl(),
-    amount: new FormControl(),
+    invoice_amount: new FormControl(),
     due_date: new FormControl()
  });
 
-  constructor(private viewbuyersService:ViewbuyersService, private http: HttpClient) {}
-  // postInvoice(){
-  //   console.warn(this.uploadForm.value);
-  //   console.log(this.uploadForm)
-  //   console.warn(this.uploadForm.value);
-  //   this.httpClient.post(`${environment.apiUrl}/invoice`,{
-  //   }).subscribe(result => {
-  //     console.log( result );
-  // });
-  // }
+  constructor(private viewbuyersService:ViewbuyersService, 
+    private httpClient: HttpClient,private router: Router,) {}
   ngOnInit() {
     this.viewbuyersService.getBuyers().subscribe( res => {
       this.buyers = res
     }, error => console.log(error))
   }
-  postInvoice(){
-    this.http.post(`${environment.apiUrl}/invoice`, this.uploadForm).subscribe(status=> console.log(JSON.stringify(status)));
+  // convenience getter for easy access to form fields
+  get f() { return this.uploadForm.controls; }
+    postInvoice(){
+    this.submitted = true;
+    if (this.uploadForm.invalid) {
+      return;
   }
+    console.log(this.uploadForm.value)
+    this.httpClient.post(`${environment.apiUrl}/invoice`, this.uploadForm.value,{
+    }).subscribe(result => {
+      console.log( result );
+  });
+  // this.router.navigate(['/invoice']);
+  }
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+  isShown: boolean = false ; // hidden by default
+
+
+toggleShow() {
+
+this.isShown = ! this.isShown;
+
+}
 }
